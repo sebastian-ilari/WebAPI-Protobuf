d81940b63548c055da40dddbefda006074013007
sebastian-ilari <sebahex@gmail.com>
2015-04-05 14:59:10 +0100

    Created a menu that performs different operations

--- README.md
+++ README.md
@@ -8,7 +8,5 @@ http://localhost:49916/api/fiml/1
 
 ### ToDos
 
-1. Improve the information returned by the client
-  * Remove the Async behavior
-  * Create a menu with the following options: Get one film and display its properties; Call films and compare return times on JSON and Protobuf
+1. Check why the timers are taking the same time
 2. Add heavy image to the data model?
--- WebClientProtobuf/Program.cs
+++ WebClientProtobuf/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Model;
@@ -10,36 +12,114 @@ namespace WebClientProtobuf
     {
         static void Main(string[] args)
         {
-            HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:49916/") };
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+            Console.WriteLine("1. Get one sample film");
+            Console.WriteLine("2. Compare formats with all films");
+            Console.Write("\nChoose an option: ");
+            ConsoleKeyInfo console = Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+            switch (console.KeyChar)
+            {
+                case '1':
+                    GetOneFilm();
+                    break;
 
-            HttpResponseMessage response = client.GetAsync("api/film/1").Result; 
-            
+                case '2':
+                    CompareFormats();
+                    break;
+                default:
+                    Console.WriteLine("\n\nInvalid character");
+                    Console.ReadKey();
+                    break;
+            } 
+        }
+
+        protected static void GetOneFilm()
+        {
+            HttpClient client = GetProtobufClient();
+            HttpResponseMessage response = client.GetAsync("api/film/1").Result;
+
             if (response.IsSuccessStatusCode)
             {
                 // Parse the response body. Blocking!
                 Film film = response.Content.ReadAsAsync<Film>(
-                    new[] { new ProtoBufFormatter()}).Result;
+                    new[] { new ProtoBufFormatter() }).Result;
                 Console.WriteLine("Id = {0}\r\n" +
                                   "Brand = {1}\r\n" +
                                   "Model = {2}\r\n" +
                                   "Color = {3}\r\n" +
                                   "Frames = {4}\r\n" +
-                                  "Size = {5}mm\r\n", 
+                                  "Size = {5}mm\r\n",
                                   film.Id,
                                   film.Brand,
                                   film.Model,
                                   film.Color,
                                   film.Frames,
-                                  film.Size);               
+                                  film.Size);
+                Console.ReadKey();
             }
             else
             {
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             }
+            Console.ReadKey();
+        }
 
-            Console.ReadLine();
-        }        
+        protected static void CompareFormats()
+        {
+            HttpClient protobufClient = GetProtobufClient();
+            Stopwatch protobufStopwatch = Stopwatch.StartNew();
+            HttpResponseMessage protobufResponse = protobufClient.GetAsync("api/film").Result;
+            if (protobufResponse.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                IList<Film> film = protobufResponse.Content.ReadAsAsync<IList<Film>>(
+                    new[] { new ProtoBufFormatter() }).Result;
+            }
+            else
+            {
+                Console.WriteLine("Protobuf service failed. {0} ({1})", 
+                                    (int)protobufResponse.StatusCode, 
+                                    protobufResponse.ReasonPhrase);
+            }
+            protobufStopwatch.Stop();
+            TimeSpan protobufTime = protobufStopwatch.Elapsed;
+            Console.WriteLine("Protobuf time = {0}", protobufTime);
+
+            
+            HttpClient jsonClient = GetJSONClient();
+            Stopwatch jsonStopwatch = Stopwatch.StartNew();
+            HttpResponseMessage jsonResponse = jsonClient.GetAsync("api/film").Result;
+            if (jsonResponse.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                IList<Film> film = jsonResponse.Content.ReadAsAsync<IList<Film>>().Result;
+            }
+            else
+            {
+                Console.WriteLine("JSON service failed. {0} ({1})",
+                                    (int)jsonResponse.StatusCode,
+                                    jsonResponse.ReasonPhrase);
+            }
+            jsonStopwatch.Stop();
+            TimeSpan jsonTime = protobufStopwatch.Elapsed;
+            Console.WriteLine("JSON time = {0}", jsonTime);
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+
+        protected static HttpClient GetProtobufClient()
+        {
+            HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:49916/") };
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+            return client;
+        }
+
+        protected static HttpClient GetJSONClient()
+        {
+            HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:49916/") };
+            return client;
+        }
     }
 }
